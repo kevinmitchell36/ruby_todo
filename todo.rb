799430311b965eb5e0961ed078e1725de717faa0
Kevin <kevingm31@gmail.com>
2017-11-07 12:39:40 -0600

    Add attr_readers/initializers/add task

--- todo.rb
+++ todo.rb
@@ -1,9 +1,29 @@
 class List
+  attr_reader :all_tasks
+
+  def initialize
+    @all_tasks = []
+  end
+
+  def add(task)
+    all_tasks << task
+  end
 end
 
+class Task
+  attr_reader :description
+
+  def initialize(description)
+    @description = description
+  end
+end
 
 
 if __FILE__ == $PROGRAM_NAME
   my_list = List.new
   puts 'You created a new list!'
+  my_list.add(Task.new('Do the dishes'))
+  
+
 end
+
