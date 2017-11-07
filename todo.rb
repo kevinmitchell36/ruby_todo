f3121adb0ff9f60a0e30c1ec74f1d73dd7b03ff5
Kevin <kevingm31@gmail.com>
2017-11-07 15:50:21 -0600

    Add menu and promptable modules

--- todo.rb
+++ todo.rb
@@ -1,3 +1,28 @@
+# Menu
+module Menu
+  def menu
+    puts 'Welcome to your todo list!'
+    puts 'Please select an option:'
+    puts '1) Add'
+    puts '2) Show'
+    puts '3) Quit'
+  end
+
+  def show
+    menu
+  end
+end
+
+# Prompts user
+module Promptable
+  def prompt(message = 'What would you like to do?', symbol = ':>')
+    print message
+    print symbol
+    gets.chomp
+  end
+end
+
+# List
 class List
   attr_reader :all_tasks
 
@@ -14,6 +39,7 @@ class List
   end
 end
 
+# Task
 class Task
   attr_reader :description
 
@@ -22,11 +48,32 @@ class Task
   end
 end
 
-
 if __FILE__ == $PROGRAM_NAME
+  include Menu
+  include Promptable
   my_list = List.new
-  puts 'You created a new list!'
-  my_tasks = my_list.add(Task.new('Do the dishes'))
-  p my_list.show
+  puts 'Please choose from the following list'
+  until ['q'].include?(user_input = prompt(show).downcase)
+    case user_input
+    when '1'
+      my_list.add(prompt('What is the task you want to accomplish?'))
+    when '2'
+      puts my_list.show
+    else
+      puts 'I did not understand that'
+    end
+  end
+  puts 'Thank you for using the todo list'
 end
+
+
+
+
+
+
+
+
+
+
+
 
